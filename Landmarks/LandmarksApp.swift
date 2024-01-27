f33614317c935521f67443634d22c3ee0cb7965e
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-01-27 10:02:32 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 宮川義之助 on 2024/01/27.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
